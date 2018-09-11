249282683984ba0efd6cacfebf7eff7ccb5d7687
JackReis01 <jackreis2019@gmail.com>
2018-09-11 09:32:05 -0500

    updated text

--- Chapter 3/src/exercises/FahrenheitToCelcius.java
+++ Chapter 3/src/exercises/FahrenheitToCelcius.java
@@ -1,10 +1,15 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class FahrenheitToCelcius {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		
 	}
 
 }
