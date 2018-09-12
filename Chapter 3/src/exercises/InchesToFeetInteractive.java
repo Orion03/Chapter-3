ada189417cda0349237d39babd1a69ea7de1ee6c
JackReis01 <jackreis2019@gmail.com>
2018-09-12 08:59:47 -0500

    updated text

--- Chapter 3/src/exercises/InchesToFeetInteractive.java
+++ Chapter 3/src/exercises/InchesToFeetInteractive.java
@@ -6,20 +6,20 @@ class InchesToFeetInteractive
 {    
   public static void main(String[] args) 
   {  
-     final int INCHES_IN_FOOT = 12;
      int inches;
-     int feet;
-     int inchesLeft;
      Scanner input = new Scanner(System.in);
+
      System.out.print("Enter inches >> ");
-     inches = input.nextInt();
-     feet = inches / INCHES_IN_FOOT;
-     inchesLeft = inches % INCHES_IN_FOOT;
-     System.out.println(inches + " inches is " +
-        feet + " feet and " + inchesLeft + " inches"); 
+ 
   }
   
-  public static void displayConversion() {
-	  
+  public static void displayConversion(int inches) {
+	  final int INCHES_IN_FOOT = 12;
+	  int feet;
+	  int inchesLeft;
+	  feet = inches / INCHES_IN_FOOT;
+	  inchesLeft = inches % INCHES_IN_FOOT;
+	  System.out.println(inches + " inches is " +
+	     feet + " feet and " + inchesLeft + " inches");
   }
 }
