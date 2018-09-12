dfb06de7b9a6cf00368c67daba0bc38a33239ae9
JackReis01 <jackreis2019@gmail.com>
2018-09-12 08:46:57 -0500

    updated text

--- Chapter 3/src/exercises/QuartsToGallonsInteractive.java
+++ Chapter 3/src/exercises/QuartsToGallonsInteractive.java
@@ -6,25 +6,13 @@ public class QuartsToGallonsInteractive {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		final int quartsInGallons = 4;
-		
 		int quartsNeeded;
-		int gallonsNeeded;
-		int extraQuarts;
+		
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter quarts needed>>>");
 		quartsNeeded = input.nextInt();
-		
-		gallonsNeeded = quartsNeeded / quartsInGallons;
-		extraQuarts = quartsNeeded % quartsInGallons;
-		
-		
-		System.out.println("You have " + gallonsNeeded + " gallons and " 
-		+ extraQuarts + " quarts left over");
-		
 		displayConversion(quartsNeeded);
 	}
 	
