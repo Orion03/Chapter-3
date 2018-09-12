fe8ae3524ea8f14042891c09ff89b6bd52922a15
JackReis01 <jackreis2019@gmail.com>
2018-09-12 09:41:46 -0500

    updated text

--- Chapter 3/src/youDoIt/ParadiseInfo2.java
+++ Chapter 3/src/youDoIt/ParadiseInfo2.java
@@ -22,6 +22,10 @@ public class ParadiseInfo2
 		displayInfo();
 		
 		savings = computeDiscountInfo(price, discount);
+		
+		System.out.println("Special this week on any service over " + price);
+		System.out.println("Discount of " + discount + " percent.");
+		System.out.println("That's a savings of at least $" + savings);
 	}
 	
 	public static void displayInfo() {
