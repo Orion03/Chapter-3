cc90853b5246ffb648b6849ee2c931b593c974ed
JackReis01 <jackreis2019@gmail.com>
2018-09-13 09:46:57 -0500

    updated text

--- Chapter 3/src/youDoIt/SpaService.java
+++ Chapter 3/src/youDoIt/SpaService.java
@@ -3,11 +3,32 @@ package youDoIt;
 import java.util.Scanner;
 
 public class SpaService {
+	private String serviceDescription;
+	
+	private double price;
+	
+	public void setServiceDescription(String Service)
+	{
+		serviceDescription = service;
+	}
+	
+	public void setPrice(double pr)
+	{
+		price = pr;
+	}
+	
+	public String getServiceDescription()
+	{
+		return serviceDescription;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
 	}
 
 }
