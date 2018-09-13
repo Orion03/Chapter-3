a1e19c3a5705b6b040012bdb757ba91ca0e9727a
JackReis01 <jackreis2019@gmail.com>
2018-09-13 08:57:43 -0500

    updated text

--- Chapter 3/src/exercises/NumbersDemo.java
+++ Chapter 3/src/exercises/NumbersDemo.java
@@ -4,7 +4,20 @@ public class NumbersDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int x = 5;
+		int y = 12;
+		
+	}
+	
+	public static void displayTwiceTheNumber(int x, int y) {
+		
+		int x2;
+		int y2;
+		
+		x2 = x * 5;
+		y2 = y * 5;
+		System.out.println("x = " + x2 + " y = " + y2);
 	}
 
 }
