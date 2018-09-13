bf67cf356da50dd3bfa388906bdb07f647b0e03f
JackReis01 <jackreis2019@gmail.com>
2018-09-13 09:23:48 -0500

    updated text

---
+++ Chapter 3/src/exercises/Percentages.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class Percentages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 5;
+		double y = 12;
+		
+		computePercent(x, y);
+		
+	}
+	
+	public static void computePercent(double x, double y) {
+		
+		double compute;
+		
+		compute = x / y;
+	}
+
+}
