d31dad697a66637868933732ceba3d3c12a46604
JackReis01 <jackreis2019@gmail.com>
2018-09-14 08:44:34 -0500

    updated text

---
+++ Chapter 3/src/exercises/TestLbls.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class TestLbls {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		double pounds;
+		
+		System.out.println("How many pounds do you weigh >> ");
+		pounds = input.nextDouble();
+		
+		poundsToKilograms(pounds);
+
+	}
+	
+	public static void poundsToKilograms(double pounds) {
+		
+	}
+
+}
