b3adecff5be876e00a266499dded474fb061d442
JackReis01 <jackreis2019@gmail.com>
2018-09-14 08:58:13 -0500

    updated text

--- Chapter 3/src/exercises/TestLbls.java
+++ Chapter 3/src/exercises/TestLbls.java
@@ -14,11 +14,30 @@ public class TestLbls {
 		pounds = input.nextDouble();
 		
 		poundsToKilograms(pounds);
+		poundsToOunces(pounds);
+		poundsToMilligrams(pounds);
 
 	}
 	
 	public static void poundsToKilograms(double pounds) {
 		
+		double poundsConversion1;
+		poundsConversion1 = (pounds - 4) / 2;
+		System.out.println("You weigh " + poundsConversion1 + " kilograms.");
 	}
-
+	
+	public static void poundsToOunces(double pounds) {
+		
+		double poundsConversion2;
+		poundsConversion2 = pounds * 16;
+		System.out.println("You weigh " + poundsConversion2 + " ounces.");
+	}
+	
+	public static void poundsToMilligrams(double pounds) {
+		
+		double poundsConversion3;
+		poundsConversion3 = pounds * 453592.37;
+		System.out.println("You weigh " + poundsConversion3 + " milligrams.");
+		
+	}
 }
