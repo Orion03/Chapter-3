f3c1440a431a14673569d44288e7c8d49921d8a1
JackReis01 <jackreis2019@gmail.com>
2018-09-17 08:53:06 -0500

    updated text

--- Chapter 3/src/exercises/BookstoreCredit.java
+++ Chapter 3/src/exercises/BookstoreCredit.java
@@ -1,10 +1,36 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class BookstoreCredit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String name;
+		double GPA;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your full name >> ");
+		name = input.nextLine();
+		
+		System.out.println("What is your grade point average >> ");
+		GPA = input.nextDouble();
+		
+		displayMessage(name, GPA);
+	}
+	
+	public static void displayMessage(String name, double GPA) {
+		
+		double yourCredit;
+		
+		yourCredit = GPA * 10;
+		
+		System.out.println("Your name: " + name);
+		System.out.println("Your GPA: " + GPA);
+		System.out.println("Your total bookstore credit is $" + yourCredit + "!");
+		
 	}
 
 }
